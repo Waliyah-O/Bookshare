965b82dbe50f96c05c6254969477a6f488306066
Waliyah-O <waliyah.okunade@i-academy.org>
2023-12-08 13:59:08 +0100

    typefilters

--- src/server/server.js
+++ src/server/server.js
@@ -26,7 +26,7 @@ createServer({
       description:
         "Lagos State yellow buses, often referred to as 'Danfo' buses, are a distinctive and iconic mode of public transportation in Lagos, Nigeria. These buses are an integral part of the city's transportation system, serving as a primary means of commuting for millions of Lagosians.",
       imageUrl: Bookshare1,
-      type: "books",
+      type: "book",
       hostId: "123",
     });
     server.create("book", {
@@ -36,7 +36,7 @@ createServer({
       description:
         "Beach Bum is a van inspired by surfers and travelers. It was created to be a portable home away from home, but with some cool features in it you won't find in an ordinary camper.",
       imageUrl: Bookshare2,
-      type: "articles",
+      type: "article",
       hostId: "123",
     });
     server.create("book", {
@@ -46,7 +46,7 @@ createServer({
       description:
         "Reliable Red is a van that was made for traveling. The inside is comfortable and cozy, with plenty of space to stretch out in. There's a small kitchen, so you can cook if you need to. You'll feel like home as soon as you step out of it.",
       imageUrl: Bookshare3,
-      type: "novels",
+      type: "novel",
       hostId: "456",
     });
     server.create("book", {
@@ -56,7 +56,7 @@ createServer({
       description:
         "Dreamfinder is the perfect van to travel in and experience. With a ceiling height of 2.1m, you can stand up in this van and there is great head room. The floor is a beautiful glass-reinforced plastic (GRP) which is easy to clean and very hard wearing. A large rear window and large side windows make it really light inside and keep it well ventilated.",
       imageUrl: Bookshare4,
-      type: "books",
+      type: "book",
       hostId: "789",
     });
     server.create("book", {
@@ -66,7 +66,7 @@ createServer({
       description:
         "The Cruiser is a van for those who love to travel in comfort and luxury. With its many windows, spacious interior and ample storage space, the Cruiser offers a beautiful view wherever you go.",
       imageUrl: Bookshare5,
-      type: "novels",
+      type: "novel",
       hostId: "789",
     });
     server.create("book", {
@@ -76,7 +76,7 @@ createServer({
       description:
         "With this van, you can take your travel life to the next level. The Green Wonder is a sustainable vehicle that's perfect for people who are looking for a stylish, eco-friendly mode of transport that can go anywhere.",
       imageUrl: Bookshare6,
-      type: "articles",
+      type: "article",
       hostId: "123",
     });
 
@@ -87,7 +87,7 @@ createServer({
       description:
         "This van elevates your travel experience to new heights. The Red Thunder represents an environmentally conscious vehicle, ideal for individuals seeking a fashionable and eco-friendly means of transportation capable of traversing any terrain.",
       imageUrl: Bookshare7,
-      type: "articles",
+      type: "article",
       hostId: "456",
     });
     server.create("book", {
@@ -97,7 +97,7 @@ createServer({
       description:
         "The Green Goblin is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!",
       imageUrl: Bookshare8,
-      type: "novels",
+      type: "novel",
       hostId: "123",
     });
 
@@ -108,7 +108,7 @@ createServer({
       description:
         "The Green Goblin is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!",
       imageUrl: Bookshare9,
-      type: "novels",
+      type: "novel",
       hostId: "123",
     });
 
@@ -119,7 +119,7 @@ createServer({
       description:
         "The Green Goblin is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!",
       imageUrl:Bookshare10,
-      type: "books",
+      type: "book",
       hostId: "456",
     });
 
