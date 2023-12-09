fa172fd115819780d651ea080219acd343e5bf95
Brown <memoyebrown@gmail.com>
2023-12-09 22:51:14 +0100

    resolve merge conflicts

--- src/pages/Host/HostBookPhotos.jsx
+++ src/pages/Host/HostBookPhotos.jsx
@@ -6,13 +6,9 @@ const HostVanPhotos = () => {
   return (
     <div>
       <img
-<<<<<<< HEAD
-        className="host-van-detail-image"
-=======
         className="host-book-detail-image"
->>>>>>> 16289cf57e600de90f64c61c7f36fddaeb0ad680
-        src={currentVan.imageUrl}
-        alt={`Photo of ${currentVan.name}`}
+        src={ currentVan.imageUrl }
+        alt={ `Photo of ${currentVan.name}` }
       />
     </div>
   );
