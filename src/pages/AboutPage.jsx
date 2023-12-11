c7b1d4c06c96ec523d64253c2b4fc50b651be0d9
Taiwo Raji <129079770+akwadhana@users.noreply.github.com>
2023-12-11 16:15:09 +0100

    stiling, hide/show password

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,7 +6,7 @@ import gif3 from "../assets/gif/reading-more-books.gif";
 function AboutPage() {
   const bookShare = (
     <strong>
-      BOOK<span style={ { color: "#ff8c38" } }>SHARE</span>
+      BOOK<span style={{ color: "#1A5276" }}>SHARE</span>
     </strong>
   );
   return (
@@ -19,33 +19,30 @@ function AboutPage() {
             passionate bibliophiles dedicated to bringing the world of
             literature to your fingertips.
           </p>
-          <img src={ gif } className="about-hero-image" alt="" />
-        </div>{ " " }
+          <img src={gif} className="about-hero-image" alt="" />
+        </div>{" "}
         <div className="about-page-content">
           <div className="about-section2">
-            <img src={ gif2 } alt="" />
+            <img src={gif2} alt="" />
             <div>
               <div class="our-mission">
-              <h3>Our Mission</h3>
-              <h3>Our Mission</h3>
-              </div>
+                <h3 style={{ color: "#1A5276", fontSize: '3em' }}>Our Mission</h3>
               
+              </div>
+
               <p className="about-mission-content">
                 At {bookShare}, our mission is to curate a diverse collection of
-                books that cater to every taste and interest. We believe that
-                everyone deserves access to great literature, and our carefully
-                selected catalog reflects our commitment to quality and
-                diversity. Whether you're a seasoned reader or just beginning
-                your literary journey, we aim to be your trusted companion on
-                the road to discovery.
+                books that cater to every taste and interest. Whether you're a
+                seasoned reader or just beginning your literary journey, we aim
+                to be your trusted companion on the road to discovery.
               </p>
             </div>
           </div>
           <div className="about-section3">
             <div className="about-section3-content">
-              <div class="contents">
-              <h3 >What Sets Us Apart</h3>
-              <h3 >What Sets Us Apart</h3>
+              <div className="contents">
+                <h3 style={{ color: "#1A5276", fontSize: '3em' }}>What Sets Us Apart</h3>
+                
               </div>
               <p className="us-apart">
                 Curated Selection Our team of passionate curators meticulously
@@ -54,39 +51,32 @@ function AboutPage() {
                 selection that spans genres and cultures.
               </p>
             </div>
-            <img className="about-section3" src={ gif3 } alt="" />
+            <img className="about-section3" src={gif3} alt="" />
           </div>
           <div className="about-contact">
-          {/* <h3 className="about-contact-us">Contact Us</h3> */}
-          <p className="about-contents" >
-            Have questions or suggestions? We'd love to hear from you! Reach out
-            to our dedicated support team at [support@yourbookstore.com] or
-            visit us{" "}
-           
-            <Link to="/contact" style={{ color: "#ff8c38" }}>
-              here
-            </Link>
-            .
-          </p>
+            <h3 style={{ color: "#1A5276", fontSize: '3em' }}>Contact Us</h3>
+            <p>
+              Have questions or suggestions? We'd love to hear from you! Reach
+              out to our dedicated support team at [support@yourbookstore.com]
+              or visit us{" "}
+              <Link to="/contact" style={{ color: "#ff8c38" }}>
+                here
+              </Link>
+              .
+            </p>
           </div>
-          <h2 className="thank-you ">
+          <h2>
             Thank you for choosing {bookShare} as your literary companion. Happy
             reading!
-          </h2> ``
+          </h2>
         </div>
-        <section>
-  <div class="content">
-    <h2 className="content-text">contact us</h2>
-    <h2>contact us</h2>
-  </div>
-</section>
-        <div className="about-page-cta">
-          <h2>Your books are ready.</h2>
-          <div className="about-buttons">
-            <Link className="link-button" to="/books">
-              Explore
-            </Link>
-          </div>
+      </div>{" "}
+      <div className="about-page-cta">
+        <h2>Your books are ready.</h2>
+        <div className="about-buttons">
+          <Link className="link-button" to="/books">
+            Explore
+          </Link>
         </div>
       </div>
     </div>
