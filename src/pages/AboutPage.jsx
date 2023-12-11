c49a2f35dc0e0da4192cb369b84dc2e57026ab97
Damola <122807356+Damola10@users.noreply.github.com>
2023-12-11 15:15:24 +0100

    changes made

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,66 +6,28 @@ import gif3 from "../assets/gif/reading-more-books.gif";
 function AboutPage() {
   const bookShare = (
     <strong>
-      BOOK<span style={ { color: "#ff8c38" } }>SHARE</span>
+      BOOK<span style={{ color: "#ff8c38" }}>SHARE</span>
     </strong>
   );
   return (
-<<<<<<< HEAD
-    <div className="about-page-container" style={{ display: "flex" }}>
-      <div className="about-page-content">
-        <h1>
-          About <span style={{ color: "#ff8c38" }}>us</span>
-        </h1>
-        <p style={{ fontWeight: "500" }}>
-          At Bookshare, we are passionate advocates for the transformative power
-          of literature and education. Committed to fostering a community of
-          lifelong learners, we provide a platform where individuals can embark
-          on intellectual journeys, exploring diverse stories and acquiring
-          knowledge that enriches the mind. Our curated collection of books is
-          more than a mere assortment of titles; it's a carefully selected
-          tapestry of thought-provoking narratives and educational resources.
-          With a dedication to accessibility and inclusivity, we strive to
-          bridge gaps and connect readers, learners, and educators. Founded on
-          the belief in the extraordinary impact of shared stories, we go beyond
-          the traditional boundaries of a bookstore. Bookshare is a vibrant
-          space where the love for learning meets the entrepreneurial spirit,
-          empowering individuals to turn their passion for literature into
-          educational and financial opportunities. Join us in creating a legacy
-          of shared stories that inspire, empower, and transform lives. At
-          Bookshare, every page tells a story, and every reader contributes to a
-          community of knowledge seekers. Together, let's shape a future where
-          access to literature and education knows no limits.
-        </p>
-      </div>
-      <img
-        src={bgImg}
-        className="about-hero-image"
-        alt=""
-        style={{
-          width: "50%",
-          marginRight: "1.5rem",
-          borderRadius: "2rem,auto",
-        }}
-      />
-=======
     <div className="about-page-container">
       <div className="about-page-wrapper">
         <div className="about-hero-wrapper">
           <p>
-            Welcome to { bookShare }, where the love for books meets the joy of
+            Welcome to {bookShare}, where the love for books meets the joy of
             reading! We are more than just an online bookstore; we are
             passionate bibliophiles dedicated to bringing the world of
             literature to your fingertips.
           </p>
-          <img src={ gif } className="about-hero-image" alt="" />
-        </div>{ " " }
+          <img src={gif} className="about-hero-image" alt="" />
+        </div>{" "}
         <div className="about-page-content">
           <div className="about-section2">
-            <img src={ gif2 } alt="" />
+            <img src={gif2} alt="" />
             <div>
               <h3>Our Mission</h3>
               <p>
-                At { bookShare }, our mission is to curate a diverse collection of
+                At {bookShare}, our mission is to curate a diverse collection of
                 books that cater to every taste and interest. We believe that
                 everyone deserves access to great literature, and our carefully
                 selected catalog reflects our commitment to quality and
@@ -85,22 +47,23 @@ function AboutPage() {
                 selection that spans genres and cultures.
               </p>
             </div>
-            <img className="about-section3" src={ gif3 } alt="" />
+            <img className="about-section3" src={gif3} alt="" />
           </div>
           <h3>Contact Us</h3>
           <p>
             Have questions or suggestions? We'd love to hear from you! Reach out
             to our dedicated support team at [support@yourbookstore.com] or
-            visit us{ " " }
-            <Link to="/contact" style={ { color: "#ff8c38" } }>
+            visit us{" "}
+            <Link to="/contact" style={{ color: "#ff8c38" }}>
               here
             </Link>
             .
           </p>
           <h2>
-            Thank you for choosing { bookShare } as your literary companion. Happy
+            Thank you for choosing {bookShare} as your literary companion. Happy
             reading!
-          </h2> ``
+          </h2>{" "}
+          ``
         </div>
         <div className="about-page-cta">
           <h2>Your books are ready.</h2>
@@ -111,7 +74,6 @@ function AboutPage() {
           </div>
         </div>
       </div>
->>>>>>> 1ae6e2ef3bcec9f16d53cd1f0783c3a0868e455d
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,21 +4,6 @@ import homeImg from "../assets/images/book light.jpg";
 const HomePage = () => {
   return (
     <div className="home-container">
-<<<<<<< HEAD
-      <h1>Beyond Pages: Unlock Value, Earn Reward– Welcome to Bookshare</h1>
-      <p>
-        Embark on a Literary Journey with Bookshare. Uncover the World of
-        Knowledge: Immerse Yourself in Educational Adventures at Bookshare.
-        Explore Intriguing Stories, Discover New Realms of Understanding, and
-        Nourish Your Mind. Dive into our Thoughtfully Curated Collection of
-        Books that Speak to the Essence of Learning. Immerse Yourself in the
-        Rich Diversity of Words and Ideas. Your Next Educational Journey Awaits
-        at Bookshare.
-      </p>
-      <div className="home-buttons">
-        <Link to="books">Buy a Book</Link>
-        <Link to="login">Sell your old books</Link>
-=======
       <div className="home-wrapper">
         <h1>Educating people, protecting the environment</h1>
         <div className="home-buttons">
@@ -32,7 +17,6 @@ const HomePage = () => {
           on those in need. Together, let's rewrite stories and build a brighter
           future for all.
         </p>
->>>>>>> 1ae6e2ef3bcec9f16d53cd1f0783c3a0868e455d
       </div>
     </div>
   );
